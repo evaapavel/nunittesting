f1e5b99a92d2c3e43dc1318deffeca0d134a36fe
Pavel Foltyn <pavel.foltyn@volny.cz>
2020-04-19 05:50:27 +0200

    CHANGE NUnit - Add the AppUrl type.

--- NUnitTestExamples/SimpleNUnitTests/Program.cs
+++ NUnitTestExamples/SimpleNUnitTests/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace SimpleNUnitTests
 {
-    class Program
+
+
+
+    /// <summary>
+    /// Exposes the main application entry point.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+
+
+
+        /// <summary>
+        /// Main application entry point.
+        /// </summary>
+        /// <param name="args">Command line parameters.</param>
+        public static void Main(string[] args)
         {
+
+            // Give some debug info.
+            Console.WriteLine("Hi there!");
+
+            // Give more info.
+            Console.WriteLine("Hello, mate!");
+
+            // Give even more info.
+            Console.WriteLine("Yep, I'm here, buddy!");
+
+            // Give one more log line.
+            Console.WriteLine("Keep thinking...");
+
+            // Leave out a blank line.
+            Console.WriteLine();
+
+            // Prepare a URL helper object.
+            AppUrl appUrl = new AppUrl();
+
+            // Display the root URL.
+            Console.WriteLine("The application root is:     {0}", appUrl.getRootUrl());
+
+            // Display the projects' URL.
+            Console.WriteLine("The project list page is:    {0}", appUrl.getProjectUrl());
+
+            // Display the contacts' URL.
+            Console.WriteLine("The contacts' page is:       {0}", appUrl.getContactUrl());
+
+            // Leave out a blank line.
+            Console.WriteLine();
+
+            // Stop the execution until the user presses ENTER.
+            Console.Write("Press ENTER to finish the app.");
+            Console.ReadLine();
+
         }
+
+
+
     }
+
+
+
 }
